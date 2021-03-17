cd3b43c2271001aec4e0bb59223fad9cf8567200
Luis A. Martín <lorgam@gmail.com>
2021-03-17 21:42:37 +0100

    Categories after header

--- plugins/latiendadeljo/index.php
+++ plugins/latiendadeljo/index.php
@@ -12,6 +12,7 @@ $active_plugins = get_option('active_plugins');
 if (in_array('elementor/elementor.php', $active_plugins) && in_array('woocommerce/woocommerce.php', $active_plugins)) { // @TODO: This should be a requirement of the plugin
   add_action('init', 'load_elementor_widget');
   add_action('wp_enqueue_scripts', 'enqueue_jo_scripts');
+  add_action('astra_header_after', 'jo_header_after');
 }
 
 function load_elementor_widget() {
@@ -23,3 +24,27 @@ function enqueue_jo_scripts() {
   wp_enqueue_style('latiendadeljo', plugins_url('styles.css', __FILE__), false);
 }
 
+function jo_header_after() {
+    $categories = get_terms( ['taxonomy' => 'product_cat' ] );
+
+    if (!empty($categories)): ?>
+      <div class="jo-categories-container">
+        <div class="jo-categories-main">
+
+        <?php foreach ($categories as $category):
+          $thumbnail = get_term_meta( $category->term_id, 'thumbnail_id', true );
+          $img = wp_get_attachment_url( $thumbnail ); ?>
+
+          <div class="category">
+            <a href="<?= get_term_link($category->term_id) ?>" class="cat-link">
+              <div class="img" style="background-image:url(<?= $img ?>)"></div>
+              <span class="name"><?= $category->name ?></span>
+            </a>
+          </div>
+
+        <?php endforeach; ?>
+        </div>
+      </div>
+
+    <?php endif;
+  }
