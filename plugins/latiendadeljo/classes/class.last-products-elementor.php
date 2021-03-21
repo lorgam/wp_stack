e1cfe39eb4b3f41000d015d37ab8120ce23aa025
Luis A. Martín <lorgam@gmail.com>
2021-03-21 21:07:00 +0100

    Last products same style as astra

--- plugins/latiendadeljo/classes/class.last-products-elementor.php
+++ plugins/latiendadeljo/classes/class.last-products-elementor.php
@@ -33,14 +33,13 @@ class Elementor_Last_Products_Jo_Widget extends \Elementor\Widget_Base {
     $query = new WP_Query($args);
 
     if ($query->have_posts()): ?>
-      <ul class="jo-last-posts">
+      <div class="woocommerce"><ul class="products columns-4">
         <?php while ($query->have_posts()):
           $query->the_post();
           $productId = $query->post->ID;
           $product = wc_get_product($productId);
           $productLink = $product->get_permalink();
           $price = wc_get_price_to_display($product);
-          $priceDisplay = ($price ? number_format($price, 2, ',', '.') . get_woocommerce_currency_symbol() : '');
           // Images of the product
           $img = $product->get_image_id();
           $img = ($img ? wp_get_attachment_image_src($img, [300, 300])[0] : wc_placeholder_img_src());
@@ -49,25 +48,40 @@ class Elementor_Last_Products_Jo_Widget extends \Elementor\Widget_Base {
             // @TODO: Grouped products
             $addToCartLink = "?add-to-cart=$productId";
             $addToCartText = __('Añadir al carrito', 'latiendadeljo');
-            $addToCartClass = "jo-add-to-cart";
+            $addToCartClass = "button product_type_simple add_to_cart_button ajax_add_to_cart";
           } else {
             $addToCartLink = $productLink;
             $addToCartText = __('+ Info', 'latiendadeljo');
-            $addToCartClass = "jo-read-more";
+            $addToCartClass = "button product_type_simple";
           }
 ?>
 
-          <li class="content">
-            <a href="<?= $productLink ?>">
-              <img class="img" src="<?= $img ?>" alt="<?= the_title() ?>" />
-            </a>
-            <h3 class="title"><?= the_title() ?></h3>
-            <span class="price"><?= $priceDisplay ?></span>
+          <li class="ast-col-sm-12 ast-article-post product">
+            <div class="astra-shop-thumbnail-wrap">
+              <a href="<?= $productLink ?>" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
+                <img class="attachment-woocommerce_thumbnail size-woocommerce_thumbnail" src="<?= $img ?>" alt="<?= the_title() ?>" />
+              </a>
+            </div>
+            <div class="astra-shop-summary-wrap">
+              <a href="<?= $productLink ?>" class="ast-loop-product__link">
+                <h2 class="woocommerce-loop-product__title"><?= the_title() ?></h2>
+              </a>
+              <?php if($price): ?>
+                <span class="price">
+                  <span class="woocommerce-Price-amount amount">
+                    <bdi>
+                      <?= number_format($price, 2, ',', '.') ?>
+                      <span class="woocommerce-Price-currencySymbol"><?= get_woocommerce_currency_symbol() ?></span>
+                    </bdi>
+                  </span>
+                </span>
+              <?php endif; ?>
+            </div>
             <a class="<?= $addToCartClass ?>" href="<?= $addToCartLink ?>"><?= $addToCartText ?></a>
           </li>
 
           <?php endwhile; ?>
-      </ul>
+      </ul></div>
     <?php else:
       echo __('No hay productos', 'latiendadeljo');
     endif;
